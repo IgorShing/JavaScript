98b0a1bdd022c7d06a41636456ae6f823e6460b6
unknown <Igor_Shingaryov@EPBYMINW5157.minsk.epam.com>
2015-12-21 10:31:38 +0300

    Lesson 4 | Traverse DOM

--- LearningJQuery/scripts/main.js
+++ LearningJQuery/scripts/main.js
@@ -4,9 +4,24 @@
 
 $(function(){
 	
+	// Clears the content of a panel
+	$("#clearPanelContent").on('click', function(){
+		$(".panel-content").html('');
+	});
+	
+	var content = "New panel content";
+
+	// Clicks on the buttons
 	$(".panel-button").on('click', function(){
 		var panelId = $(this).attr('data-panelid');
 		$('#' + panelId).toggle();
+		$('#' + panelId + ' .panel-content').html(content);
 	});
 
+	// Clicks on list elements
+	$('li').on('click', function(){
+		$(this).removeClass('special')
+		$(this).siblings().addClass('special');
+	});
+	
 });
